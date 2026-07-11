d0b912c797e4f5826a05301a584bffb865957139
Javier Martinez <javier.martinezperches01@gmail.com>
2026-07-11 17:24:38 -0600

    instalacion de tailwind

--- blog-react/src/App.jsx
+++ blog-react/src/App.jsx
@@ -5,9 +5,13 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-100">
+      <h1 className="text-5xl font-bold text-blue-600">
+        ¡Tailwind funciona!
+      </h1>
+    </div>
+  )
 }
 
 export default App
