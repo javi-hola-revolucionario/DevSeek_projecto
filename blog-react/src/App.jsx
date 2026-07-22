b2332a5480749d295c8d5be9dc48faaae94ddbfb
Javier Martinez <javier.martinezperches01@gmail.com>
2026-07-21 22:52:51 -0600

    projecto devseek

--- blog-react/src/App.jsx
+++ blog-react/src/App.jsx
@@ -1,12 +1,43 @@
 import { ChatProvider } from './context/ChatContext'
 import Home from './pages/Home'
+import { useState, useEffect } from 'react'
+import { isSupabaseConfigured, supabase } from './utils/supabase'
 
-function App() {
+export default function App() {
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    if (!isSupabaseConfigured || !supabase) {
+      return
+    }
+
+    async function getTodos() {
+      const { data } = await supabase.from('todos').select()
+
+      if (data) {
+        setTodos(data)
+      }
+    }
+
+    getTodos()
+  }, [])
+
   return (
-    <ChatProvider>
-      <Home />
-    </ChatProvider>
+    <>
+      {isSupabaseConfigured ? (
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>{todo.name}</li>
+          ))}
+        </ul>
+      ) : (
+        <p style={{ padding: '1rem' }}>
+          Supabase is not configured yet. Add your environment variables to enable the data list.
+        </p>
+      )}
+      <ChatProvider>
+        <Home />
+      </ChatProvider>
+    </>
   )
 }
-
-export default App
